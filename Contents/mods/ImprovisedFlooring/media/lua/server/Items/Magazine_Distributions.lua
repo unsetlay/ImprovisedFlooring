cac757da663f9b7abf0c8e4ffca3fc255c053c6f
Orcicorn <orcicorn@gmail.com>
2022-04-18 13:35:31 -0400

    Added Fossoil specials

--- Contents/mods/ImprovisedFlooring/media/lua/server/Items/Magazine_Distributions.lua
+++ Contents/mods/ImprovisedFlooring/media/lua/server/Items/Magazine_Distributions.lua
@@ -120,6 +120,26 @@ table.insert(ProceduralDistributions.list["PizzaKitchenButcher"].items, 3);
 table.insert(ProceduralDistributions.list["PlankStashMagazine"].items, "ImprovisedFlooring.IFMagazineSpecial3");
 table.insert(ProceduralDistributions.list["PlankStashMagazine"].items, 5);
 
+-- Special 4 (Fossoil)
+table.insert(ProceduralDistributions.list["CrateBooks"].items, "ImprovisedFlooring.IFMagazineSpecial4");
+table.insert(ProceduralDistributions.list["CrateBooks"].items, 0.5);
+table.insert(ProceduralDistributions.list["FactoryLockers"].items, "ImprovisedFlooring.IFMagazineSpecial4");
+table.insert(ProceduralDistributions.list["FactoryLockers"].items, 2);
+table.insert(ProceduralDistributions.list["FilingCabinetGeneric"].items, "ImprovisedFlooring.IFMagazineSpecial4");
+table.insert(ProceduralDistributions.list["FilingCabinetGeneric"].items, 0.5);
+table.insert(ProceduralDistributions.list["GarageMechanics"].items, "ImprovisedFlooring.IFMagazineSpecial4");
+table.insert(ProceduralDistributions.list["GarageMechanics"].items, 0.5);
+table.insert(ProceduralDistributions.list["MechanicShelfBooks"].items, "ImprovisedFlooring.IFMagazineSpecial4");
+table.insert(ProceduralDistributions.list["MechanicShelfBooks"].items, 0.7);
+table.insert(ProceduralDistributions.list["OfficeCounter"].items, "ImprovisedFlooring.IFMagazineSpecial4");
+table.insert(ProceduralDistributions.list["OfficeCounter"].items, 1);
+table.insert(ProceduralDistributions.list["OfficeDeskHome"].items, "ImprovisedFlooring.IFMagazineSpecial4");
+table.insert(ProceduralDistributions.list["OfficeDeskHome"].items, 0.1);
+table.insert(ProceduralDistributions.list["PlankStashMagazine"].items, "ImprovisedFlooring.IFMagazineSpecial4");
+table.insert(ProceduralDistributions.list["PlankStashMagazine"].items, 2);
+table.insert(ProceduralDistributions.list["PostOfficeMagazines"].items, "ImprovisedFlooring.IFMagazineSpecial4");
+table.insert(ProceduralDistributions.list["PostOfficeMagazines"].items, 0.5);
+
 -- Tiles 1
 table.insert(ProceduralDistributions.list["BookstoreMisc"].items, "ImprovisedFlooring.IFMagazineTiles1");
 table.insert(ProceduralDistributions.list["BookstoreMisc"].items, 3);
