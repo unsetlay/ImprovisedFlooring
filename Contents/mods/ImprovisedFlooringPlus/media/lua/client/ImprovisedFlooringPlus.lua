7fa231d5ec1cce6537d2855ec0745b27d43a4cde
Orcicorn <orcicorn@gmail.com>
2022-05-12 09:13:52 -0400

    Fixed incorrect Delta configurations

--- Contents/mods/ImprovisedFlooringPlus/media/lua/client/ImprovisedFlooringPlus.lua
+++ Contents/mods/ImprovisedFlooringPlus/media/lua/client/ImprovisedFlooringPlus.lua
@@ -1,21 +1,22 @@
 require("ItemTweaker_Core");
 
 -- Paint now has 10 Uses per bucket
-TweakItem("Base.PaintBlack", "UseDelta", "0.1");
-TweakItem("Base.PaintBlue", "UseDelta", "0.1");
-TweakItem("Base.PaintBrown", "UseDelta", "0.1");
-TweakItem("Base.PaintCyan", "UseDelta", "0.1");
-TweakItem("Base.PaintGreen", "UseDelta", "0.1");
-TweakItem("Base.PaintGrey", "UseDelta", "0.1");
-TweakItem("Base.PaintLightBlue", "UseDelta", "0.1");
-TweakItem("Base.PaintLightBrown", "UseDelta", "0.1");
-TweakItem("Base.PaintOrange", "UseDelta", "0.1");
-TweakItem("Base.PaintPink", "UseDelta", "0.1");
-TweakItem("Base.PaintPurple", "UseDelta", "0.1");
-TweakItem("Base.PaintRed", "UseDelta", "0.1");
-TweakItem("Base.PaintTurquoise", "UseDelta", "0.1");
-TweakItem("Base.PaintWhite", "UseDelta", "0.1");
-TweakItem("Base.PaintYellow", "UseDelta", "0.1");
+TweakItem("Base.PaintBlack", "UseDelta", 0.1);
+TweakItem("Base.PaintBlue", "UseDelta", 0.1);
+TweakItem("Base.PaintBrown", "UseDelta", 0.1);
+TweakItem("Base.PaintCyan", "UseDelta", 0.1);
+TweakItem("Base.PaintGreen", "UseDelta", 0.1);
+TweakItem("Base.PaintGrey", "UseDelta", 0.1);
+TweakItem("Base.PaintLightBlue", "UseDelta", 0.1);
+TweakItem("Base.PaintLightBrown", "UseDelta", 0.1);
+TweakItem("Base.PaintOrange", "UseDelta", 0.1);
+TweakItem("Base.PaintPink", "UseDelta", 0.1);
+TweakItem("Base.PaintPurple", "UseDelta", 0.1);
+TweakItem("Base.PaintRed", "UseDelta", 0.1);
+TweakItem("Base.PaintTurquoise", "UseDelta", 0.1);
+TweakItem("Base.PaintWhite", "UseDelta", 0.1);
+TweakItem("Base.PaintYellow", "UseDelta", 0.1);
 
 -- BASE GAME: Plaster Lasts 10 uses per bucket
-TweakItem("Base.BucketPlasterFull", "UseDelta", "0.1");
+TweakItem("Base.BucketPlasterFull", "UseDelta", 0.1);
+
